2a30729c8c5cf2efc78a686d8c72175458514bc9
alexNeto <alex.neto3@hotmail.com>
2018-05-02 07:48:54 -0300

    criado representacao das tabelas do banco de dados no java

---
+++ src/main/java/org/sjc/transparencia/data/DataRepository.java
@@ -0,0 +1,31 @@
+package org.sjc.transparencia.data;
+
+public class DataRepository {
+    private Integer data_id;
+    private String mes;
+    private Integer ano;
+
+    public Integer getData_id() {
+        return data_id;
+    }
+
+    public void setData_id(Integer data_id) {
+        this.data_id = data_id;
+    }
+
+    public String getMes() {
+        return mes;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+}
---
+++ src/main/java/org/sjc/transparencia/tabela_inicial/TabelaInicialRepository.java
@@ -0,0 +1,149 @@
+package org.sjc.transparencia.tabela_inicial;
+
+public class TabelaInicialRepository {
+
+    private Integer funcionario_id;
+    private Integer data_id;
+    private String nome;
+    private String cargo;
+    private Double salario_base;
+    private Double plano_carreira;
+    private Double gratificacao;
+    private Double beneficio;
+    private Double abono;
+    private Double adiantamento;
+    private Double ferias;
+    private Double decimo_terceiro;
+    private Double abatimento;
+    private Double descontos;
+    private Double salario_bruto;
+    private Double salario_liquido;
+
+    public Integer getFuncionario_id() {
+        return funcionario_id;
+    }
+
+    public void setFuncionario_id(Integer funcionario_id) {
+        this.funcionario_id = funcionario_id;
+    }
+
+    public Integer getData_id() {
+        return data_id;
+    }
+
+    public void setData_id(Integer data_id) {
+        this.data_id = data_id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public Double getSalario_base() {
+        return salario_base;
+    }
+
+    public void setSalario_base(Double salario_base) {
+        this.salario_base = salario_base;
+    }
+
+    public Double getPlano_carreira() {
+        return plano_carreira;
+    }
+
+    public void setPlano_carreira(Double plano_carreira) {
+        this.plano_carreira = plano_carreira;
+    }
+
+    public Double getGratificacao() {
+        return gratificacao;
+    }
+
+    public void setGratificacao(Double gratificacao) {
+        this.gratificacao = gratificacao;
+    }
+
+    public Double getBeneficio() {
+        return beneficio;
+    }
+
+    public void setBeneficio(Double beneficio) {
+        this.beneficio = beneficio;
+    }
+
+    public Double getAbono() {
+        return abono;
+    }
+
+    public void setAbono(Double abono) {
+        this.abono = abono;
+    }
+
+    public Double getAdiantamento() {
+        return adiantamento;
+    }
+
+    public void setAdiantamento(Double adiantamento) {
+        this.adiantamento = adiantamento;
+    }
+
+    public Double getFerias() {
+        return ferias;
+    }
+
+    public void setFerias(Double ferias) {
+        this.ferias = ferias;
+    }
+
+    public Double getDecimo_terceiro() {
+        return decimo_terceiro;
+    }
+
+    public void setDecimo_terceiro(Double decimo_terceiro) {
+        this.decimo_terceiro = decimo_terceiro;
+    }
+
+    public Double getAbatimento() {
+        return abatimento;
+    }
+
+    public void setAbatimento(Double abatimento) {
+        this.abatimento = abatimento;
+    }
+
+    public Double getDescontos() {
+        return descontos;
+    }
+
+    public void setDescontos(Double descontos) {
+        this.descontos = descontos;
+    }
+
+    public Double getSalario_bruto() {
+        return salario_bruto;
+    }
+
+    public void setSalario_bruto(Double salario_bruto) {
+        this.salario_bruto = salario_bruto;
+    }
+
+    public Double getSalario_liquido() {
+        return salario_liquido;
+    }
+
+    public void setSalario_liquido(Double salario_liquido) {
+        this.salario_liquido = salario_liquido;
+    }
+}
